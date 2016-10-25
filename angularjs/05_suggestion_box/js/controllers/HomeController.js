c68675e467b9f8add8b39122b1a5ad6d47d50d34
sharryhong <kshopzoa15@gmail.com>
2016-10-25 23:12:11 +0900

    upvote function add

--- angularjs/05_suggestion_box/js/controllers/HomeController.js
+++ angularjs/05_suggestion_box/js/controllers/HomeController.js
@@ -1,3 +1,20 @@
 app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions){
 	$scope.posts = suggestions.posts;
+
+	$scope.addSuggestion = function(){
+		// input에 아무것도 안넣을 때 submit을 하지 않는다.
+		if( !$scope.title || $scope.title === "" ){
+			return;
+		}
+		$scope.posts.push({
+			title: $scope.title,
+			upvotes: 0,
+		});
+		// after submit, clear input text
+		$scope.title = '';
+	};
+
+	$scope.upVote = function(post){
+		post.upvotes += 1; 
+	};
 }]);
