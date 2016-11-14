017cf18eaffe8cb40c0c9769d81330b7d81fcb9e
sharryhong <kshopzoa15@gmail.com>
2016-11-14 16:20:34 +0900

    gulp, 폴더 구조 재정비

--- angularjs/hs_slido/gulpfile.js
+++ angularjs/hs_slido/gulpfile.js
@@ -1,44 +1,71 @@
 'use strict';
-var config, gulp, sass, autoprefixer, sourcemaps, g_if, uglify;
-// 환경 설정 옵션
-config = require('./gulp.config');
-// 외부에 존재하는 gulp 라고 하는 모듈 로드
-gulp = require('gulp');
-// sass 모듈을 로드
-sass = require('gulp-sass');
-// autoprefixer 모듈을 로드
-autoprefixer = require('gulp-autoprefixer');
-// Sourcemaps 모듈을 로드
-sourcemaps = require('gulp-sourcemaps');
-// gulp-if 모듈을 로드
-g_if = require('gulp-if');
-// uglify 모듈을 로드
-uglify = require("gulp-uglify");
 
-// gulp 기본 수행할 일(task) 등록
-gulp.task('default', [
-  'compile:css',
-  'watch',
-  'uglify'
-]);
+var gulp        = require('gulp'),
+	// Gulp 유틸리티
+	gutil       = require('gulp-util'),
+	gulp_if     = require('gulp-if'),
+	// 서버 환경 구성
+	browserSync = require('browser-sync').create(),
+	// Sourcemap 생성
+	sourcemaps  = require('gulp-sourcemaps'),
+	// Sass 프리프로세싱
+	sass        = require('gulp-sass'),
+	// Javascript 압축
+	uglify      = require('gulp-uglify'),
+	// Browserify 번들링
+	browserify  = require('browserify'),
+	source      = require('vinyl-source-stream'),
+	buffer      = require('vinyl-buffer'),
+	// 환경 설정
+	config      = require('./config');
 
+// 빌드 설정 (초기 값: false)
+global.process.build = false;
+
+// 기본 업무 (Default Task) 등록
+gulp.task('default', ['server', 'watch']);
+
+// 관찰(Watch) 업무
 gulp.task('watch', function() {
-  gulp.watch(config.sass.src, ['compile:css']);
+	gulp.watch( config.paths.sass.src, ['sass'] );
+	gulp.watch( config.paths.browserify.watch_files, ['browserify'] );
+	gulp.watch( config.paths.html ).on( 'change', browserSync.reload );
 });
 
-// uglify 업무 등록
-gulp.task("uglify", function(){
-  return gulp.src("src/js/*.js")
-             .pipe(uglify())
-             .pipe(gulp.dest("dist/js"));
+// 서버(Server) 업무
+gulp.task('server', ['sass'], function() {
+	browserSync.init( config.options.browserSync );
 });
 
-// Sass 업무 등록
-gulp.task('compile:css', function() {
-    return gulp.src(config.sass.src)
-               .pipe( g_if(config.sass.sourcemaps.use, sourcemaps.init() ) )
-               .pipe( sass( config.sass.options ).on('error', sass.logError) )
-               .pipe( autoprefixer() )
-               .pipe( g_if(config.sass.sourcemaps.use, sourcemaps.write(config.sass.sourcemaps.location) ) )
-               .pipe( gulp.dest(config.sass.dest) );
+// Sass → CSS 업무
+gulp.task('sass', function() {
+	gulp
+		.src( config.paths.sass.src )
+		.pipe( sourcemaps.init() )
+		.pipe( sass( config.options.sass ).on('error', sass.logError) )
+		.pipe( sourcemaps.write('./') )
+		.pipe( gulp.dest( config.paths.sass.dest ) )
+		.pipe( browserSync.stream() );
+});
+
+// Browserify 업무(빌드 실행 시)
+gulp.task('build:browserify', function() {
+	// 시스템 프로세스 변수 값을 변경해서 처리를 할겁니다.
+	global.process.build = true;
+	// console.log( global.process.build );
+	gulp.start( 'browserify' );
+});
+
+// Browserify 업무
+gulp.task('browserify', function() {
+	var bsr = browserify( config.options.browserify );
+	bsr
+		.bundle()
+		.pipe( source( config.paths.browserify.output_filename ) )
+		.pipe( buffer() )
+		.pipe( sourcemaps.init({ loadMaps: config.paths.browserify.debugging }) )
+		.pipe( gulp_if( global.process.build , uglify()) )
+		.on( 'error', gutil.log )
+		.pipe( sourcemaps.write( config.paths.browserify.map_location ) )
+		.pipe( gulp.dest( config.paths.browserify.dest ) );
 });
--- angularjs/hs_slido/src/js/app.js
+++ angularjs/hs_slido/src/js/app.js
@@ -1,3 +1,10 @@
+// require('angular');
+// require('angular-route');
+// require('angular-animate');
+// require('./controllers/HomeController');
+// require('./controllers/SuggestionController');
+// require('./services/suggestion');
+
 var app = angular.module('SuggestionBox', ['ngRoute']);
 
 app.config(function($routeProvider){
