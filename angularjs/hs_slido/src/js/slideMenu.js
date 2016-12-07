93b282c098a4e39ea73182a5b6cb269abfafccf2
sharryhong <kshopzoa15@gmail.com>
2016-12-07 22:28:42 +0900

    모바일에서 영역이 남아있어 (display:block때문) 클릭이 안되던 현상 바로잡기

--- angularjs/hs_slido/src/js/slideMenu.js
+++ angularjs/hs_slido/src/js/slideMenu.js
@@ -4,32 +4,32 @@
 	var slideMenu = new SlideMenu();
 
 	function SlideMenu() {
-		this.$menu_wrapper = null
-		this.$toggle_btn = null
-		this.$toggle_contents = null
+		this.$toggleMenu = null
+		this.$toggleBtn = null
+		this.$toggleContents = null
 	}
 
 	SlideMenu.prototype.init = function() {
-		this.$menu_wrapper = $('.side-wrap');
-		this.$toggle_btn = this.$menu_wrapper.find('button');
-		this.$toggle_contents = this.$menu_wrapper.find('.page-nav');
+		this.$toggleMenu = $('.toggle-menu');
+		this.$toggleBtn = this.$toggleMenu.find('button');
+		this.$toggleContents = $('.page-nav');
 	}
 
 	SlideMenu.prototype.initEvent = function() {
 		var objThis = this;
-		this.$toggle_btn.on('click', function(){
+		this.$toggleBtn.on('click', function(){
 				objThis.toggleMenu();
 		});
 	}
 
 	SlideMenu.prototype.toggleMenu = function() {
-		if (this.$menu_wrapper.hasClass("sliding") ) {
-			this.$toggle_contents.stop().animate({left:"0px"}, 200);
-			this.$menu_wrapper.removeClass("sliding");
+		if (this.$toggleMenu.hasClass("sliding") ) {
+			this.$toggleContents.stop().animate({left:"0px"}, 200);
+			this.$toggleMenu.removeClass("sliding");
 			console.log('1');
 		} else {
-			this.$toggle_contents.stop().animate({left:"-200px"}, 200);
-			this.$menu_wrapper.addClass("sliding");
+			this.$toggleContents.stop().animate({left:"-200px"}, 200);
+			this.$toggleMenu.addClass("sliding");
 			console.log('2');
 		}
 	}
